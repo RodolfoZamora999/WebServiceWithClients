d4cbdd0ea6ebe34fbca0a40648c4ecf03e53771f
Rodolfo Zamora <rodolfozamora999@gmail.com>
2022-02-27 19:19:51 -0800

    I improved some features in AndroidClient, Certificates and WebService

--- AndroidClient/app/src/main/java/com/rodolfozamora/MainActivity.kt
+++ AndroidClient/app/src/main/java/com/rodolfozamora/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.rodolfozamora
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import okhttp3.OkHttpClient
+import com.rodolfozamora.tls.createOkHttpSecureBuilder
 import okhttp3.Request
-import java.io.BufferedInputStream
-import java.io.InputStream
-import java.security.KeyStore
-import java.security.cert.CertificateFactory
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManagerFactory
-import javax.net.ssl.X509TrustManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +22,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadSSL() {
-        //val ssl = loadMyOwnSSLContext(this);
-        val url = "https://www.webservice.com:8443/api/user"
-        val sslC = loadOwnSSLContext(this)
-        val trustManager = loadOwnTrustManager(this)
-        val okHttpClient = OkHttpClient.Builder().sslSocketFactory(sslC.socketFactory, trustManager).build()
+        //val url = "https://www.webservice.com:8443/api/user" Todo: Solve this
+        val url = "https://10.0.2.2:8443/api/user" //Android virtual device network
+
+        val okHttpClient = createOkHttpSecureBuilder(this).build()
         val request = Request.Builder().get().url(url).build()
 
         Thread {
@@ -54,60 +44,4 @@ class MainActivity : AppCompatActivity() {
             }
         }.start()
     }
-
-}
-
-fun loadOwnSSLContext(context: Context): SSLContext {
-    //Load Cert
-    val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
-    val caInput: InputStream = BufferedInputStream(context.resources.openRawResource(R.raw.my_projects_ca))
-    val ca: X509Certificate = caInput.use {
-        cf.generateCertificate(it) as X509Certificate
-    }
-
-    //Create a keystore
-    val keyStoreType = KeyStore.getDefaultType()
-    val keyStore = KeyStore.getInstance(keyStoreType).apply {
-        load(null, null)
-        setCertificateEntry("ca", ca)
-    }
-
-    //Create a TrustManager
-    val tmfAlgorithm: String = TrustManagerFactory.getDefaultAlgorithm()
-    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm).apply {
-        init(keyStore)
-    }
-
-    val sslContext: SSLContext = SSLContext.getInstance("TLS").apply {
-        init(null, tmf.trustManagers, null)
-    }
-
-    caInput.close()
-    return sslContext
-}
-
-fun loadOwnTrustManager(context: Context): X509TrustManager {
-    //Load Cert
-    val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
-    val caInput: InputStream = BufferedInputStream(context.resources.openRawResource(R.raw.my_projects_ca))
-    val ca: X509Certificate = caInput.use {
-        cf.generateCertificate(it) as X509Certificate
-    }
-
-    //Create a keystore
-    val keyStoreType = KeyStore.getDefaultType()
-    val keyStore = KeyStore.getInstance(keyStoreType).apply {
-        load(null, null)
-        setCertificateEntry("ca", ca)
-    }
-
-    //Create a TrustManager
-    val tmfAlgorithm: String = TrustManagerFactory.getDefaultAlgorithm()
-    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm).apply {
-        init(keyStore)
-    }
-
-    caInput.close()
-
-    return tmf.trustManagers[0] as X509TrustManager
 }
---
+++ AndroidClient/app/src/main/java/com/rodolfozamora/tls/TLSClient.kt
@@ -0,0 +1,42 @@
+package com.rodolfozamora.tls
+
+import android.content.Context
+import com.rodolfozamora.R
+import okhttp3.OkHttpClient
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
+
+fun createOkHttpSecureBuilder(context: Context): OkHttpClient.Builder {
+    val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
+    val caInput: InputStream =
+        BufferedInputStream(context.resources.openRawResource(R.raw.my_projects_ca))
+    val ca: X509Certificate = caInput.use {
+        cf.generateCertificate(it) as X509Certificate
+    }
+
+    //Create a keystore
+    val keyStoreType = KeyStore.getDefaultType()
+    val keyStore = KeyStore.getInstance(keyStoreType).apply {
+        load(null, null)
+        setCertificateEntry("ca", ca)
+    }
+
+    //Create a TrustManager
+    val tmfAlgorithm: String = TrustManagerFactory.getDefaultAlgorithm()
+    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm).apply {
+        init(keyStore)
+    }
+
+    val sslContext: SSLContext = SSLContext.getInstance("TLS").apply {
+        init(null, tmf.trustManagers, null)
+    }
+
+    return OkHttpClient.Builder()
+        .sslSocketFactory(sslContext.socketFactory, tmf.trustManagers[0] as X509TrustManager)
+}
